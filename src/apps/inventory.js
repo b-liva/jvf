19bc3fcaf36c3b26647216ae51ad9691841fdf29
b-liva <ebammo20@gmail.com>
2022-10-01 13:28:04 +0330

    fix no element error

--- src/apps/inventory.js
+++ src/apps/inventory.js
@@ -2,5 +2,8 @@ import {createApp} from "vue";
 import InventoryOutNumber from "../components/inventory/InventoryOutNumber.vue";
 
 const ioE = document.querySelector("#invOutNum")
-const invOutApp = createApp(InventoryOutNumber, {...ioE.dataset})
-invOutApp.mount("#invOutNum");
+if (ioE != null) {
+  const invOutApp = createApp(InventoryOutNumber, {...ioE.dataset})
+  invOutApp.mount("#invOutNum");
+}
+
