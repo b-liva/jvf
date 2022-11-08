54da67ddda2ece38b987ed83a9e73620fa1bd11c
b-liva <ebammo20@gmail.com>
2022-11-08 16:53:44 +0330

    Implement add and remove items

--- src/components/proforma/ProjectCostCalculator.vue
+++ src/components/proforma/ProjectCostCalculator.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {reactive} from 'vue'
+import { Button } from 'flowbite-vue'
 
 const items = reactive([
   {title: 'pph', name: 'دستمزد', unit: 0, value: 0},
@@ -27,7 +28,7 @@ const certificates = reactive({
   items: [{id: 1, value: 0, unit: 0}]
 })
 
-const test = reactive({
+const tests = reactive({
   title: 'test',
   name: 'تست',
   items: [{id: 1, value: 0, unit: 0}]
@@ -38,19 +39,29 @@ function getTotal() {
   this.items.forEach(item => total += item.unit * item.value)
   this.bearings.items.forEach(item => total += item.unit * item.value)
   this.certificates.items.forEach(item => total += item.unit * item.value)
-  this.test.items.forEach(item => total += item.unit * item.value)
+  this.tests.items.forEach(item => total += item.unit * item.value)
   return total
 }
 
+function AddNew(obj) {
+  obj.push({id: 1, value: 0, unit: 0})
+}
+function Remove(itemList, index){
+  itemList.splice(index - 1, 1)
+}
 </script>
 <template>
   <div class=""><p class="text-yellow-900 text-3xl font-bold underline">inside pc calculator...</p>
     <div class="">
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div class="inline-flex">
+          <button @click="AddNew(bearings.items)" class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-l">بیرینگ</button>
+          <button @click="AddNew(tests.items)" class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4">تست</button>
+          <button @click="AddNew(certificates.items)" class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r">گواهی نامه</button>
+        </div>
         <table class="table-auto">
           <thead>
           <tr>
-            <th>ردیف</th>
             <th>عنوان</th>
             <th>مقدار</th>
             <th>قیمت واحد</th>
@@ -60,7 +71,6 @@ function getTotal() {
           <tbody>
           <template v-for="(item, index) in items">
             <tr>
-              <td>{{ ++index }}</td>
               <td>{{ item.name }}</td>
               <td><input type="number" v-model="item.value" :id="item.title"></td>
               <td><input type="number" v-model="item.unit"></td>
@@ -69,36 +79,42 @@ function getTotal() {
           </template>
           <template v-for="(item, index) in bearings.items">
             <tr>
-              <td>{{ ++index }}</td>
               <td>بیرینگ</td>
               <td><input type="number" v-model="item.value" :id="item.title"></td>
               <td><input type="number" v-model="item.unit"></td>
               <td>{{ item.value * item.unit }}</td>
+              <td @click="Remove(bearings.items, index)">
+                <span class="red p-3 text-lg">-</span>
+              </td>
             </tr>
           </template>
-          <template v-for="(item, index) in test.items">
+          <template v-for="(item, index) in tests.items">
             <tr>
-              <td>{{ ++index }}</td>
               <td>تست</td>
               <td><input type="number" v-model="item.value" :id="item.title"></td>
               <td><input type="number" v-model="item.unit"></td>
               <td>{{ item.value * item.unit }}</td>
+              <td @click="Remove(tests.items, index)">
+                <span class="red p-3 text-lg">-</span>
+              </td>
             </tr>
           </template>
           <template v-for="(item, index) in certificates.items">
             <tr>
-              <td>{{ ++index }}</td>
               <td>گواهی</td>
               <td><input type="number" v-model="item.value" :id="item.title"></td>
               <td><input type="number" v-model="item.unit"></td>
               <td>{{ item.value * item.unit }}</td>
+              <td @click="Remove(certificates.items, index)">
+                <span class="red p-3 text-lg">-</span>
+              </td>
             </tr>
           </template>
           </tbody>
         </table>
         <button>click</button>
         {{ getTotal() }}
-      </form>
+      </div>
     </div>
   </div>
 </template>
