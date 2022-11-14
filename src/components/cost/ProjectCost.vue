73cb8dd969d6cef98dc5406bd4453de73b776a08
b-liva <ebammo20@gmail.com>
2022-11-14 16:41:50 +0330

    get order id on btn click

--- src/components/cost/ProjectCost.vue
+++ src/components/cost/ProjectCost.vue
@@ -1,8 +1,10 @@
 <script setup>
+import {ref} from 'vue';
 import {useQuery} from "@vue/apollo-composable";
 import {getOrdersByNumber} from "../../graphql/order/query/order.graphql"
 import {computed} from "vue";
 
+const orderId = ref("")
 const {result: orders, loading} = useQuery(getOrdersByNumber, {
   number: 3521
 })
@@ -10,8 +12,9 @@ const ordersModified = computed(() => orders.value?.getOrdersByNumber.edges ?? {
 </script>
 
 <template>
+  <p v-if="orderId">id: {{orderId}}</p>
   <p v-if="loading">loading...</p>
-  <p v-for="r in ordersModified" :key="r.node.id">{{r.node.number}} - {{r.node.customer.name}} - {{r.node.dateFa}}</p>
+  <p @click="orderId = r.node.id" v-for="r in ordersModified" :key="r.node.id">{{r.node.number}} - {{r.node.customer.name}} - {{r.node.dateFa}}</p>
 </template>
 
 <style scoped>
