22da08c4006df0ffadbb8f6e25ca95e0a5bd9d21
b-liva <ebammo20@gmail.com>
2022-11-14 16:47:49 +0330

    get orders by number entered

--- src/components/cost/ProjectCost.vue
+++ src/components/cost/ProjectCost.vue
@@ -5,13 +5,15 @@ import {getOrdersByNumber} from "../../graphql/order/query/order.graphql"
 import {computed} from "vue";
 
 const orderId = ref("")
-const {result: orders, loading} = useQuery(getOrdersByNumber, {
-  number: 3521
+const orderNumber = ref(0)
+const {result: orders, loading, error, refetch: refetchOrders} = useQuery(getOrdersByNumber, {
+  number: orderNumber
 })
 const ordersModified = computed(() => orders.value?.getOrdersByNumber.edges ?? {})
 </script>
 
 <template>
+  <input type="number" v-model="orderNumber" v-on:keyup.enter="refetchOrders">
   <p v-if="orderId">id: {{orderId}}</p>
   <p v-if="loading">loading...</p>
   <p @click="orderId = r.node.id" v-for="r in ordersModified" :key="r.node.id">{{r.node.number}} - {{r.node.customer.name}} - {{r.node.dateFa}}</p>
