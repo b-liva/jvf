bf7787ee072c1c512008b173eeccd9bbba3323f5
b-liva <ebammo20@gmail.com>
2022-11-15 14:02:17 +0330

    get proforma specs

--- src/components/proforma/ProformaDetails.vue
+++ src/components/proforma/ProformaDetails.vue
@@ -1,11 +1,34 @@
 <script setup>
+import {computed, watch} from "vue";
+
 const props = defineProps(['proformaId'])
+import {useQuery} from "@vue/apollo-composable";
+import {getProforma} from "../../graphql/proforma/query/proforma.graphql";
+
+
+function getProformaFn(){
+  const {result: proformaRes, loading, errors} = useQuery(getProforma,
+      () => ({
+        proformaId: props.proformaId,
+      }), {
+        prefetch: false
+      })
+  return{proformaRes, loading, errors}
+}
+
+const {proformaRes, loading, errors} = getProformaFn();
+const proforma = computed(() => proformaRes.value?.getProforma ?? {})
+watch(
+    () => props.proformaId,
+    () => {
+      getProformaFn()
+    }
+)
 
 </script>
 
 <template>
-proforma details
-  <p>inside proforma details: {{proformaId}}</p>
+  <p>inside proforma details: {{proforma.number}}</p>
 
 </template>
 
