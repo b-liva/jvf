8a8ad698e49bb9688fa33e78b4a6dbb13dedc962
b-liva <ebammo20@gmail.com>
2022-11-15 12:46:50 +0330

    Get specs by order id

--- src/components/order/OrderDetails.vue
+++ src/components/order/OrderDetails.vue
@@ -1,9 +1,35 @@
 <script setup>
+import {computed, ref, watch} from "vue";
 const props = defineProps(['orderId'])
+import {useQuery} from "@vue/apollo-composable";
+import {getSpecsById} from '../../graphql/order/query/order.graphql';
+
+function getSpecs(){
+  const {result: specsByOrder, loading, errors} = useQuery(getSpecsById,
+      () => ({
+        orderId: props.orderId,
+      }), {
+        prefetch: false
+      })
+  console.log('specs: ', specsByOrder)
+  return{specsByOrder, loading, errors}
+}
+const {specsByOrder, loading, errors} = getSpecs();
+const specs = computed(() => specsByOrder.value?.getSpecsById.edges ?? [])
+watch(
+    () => props.orderId,
+    () => {
+      console.log("order id prop change")
+      getSpecs()
+    }
+)
+
 </script>
 
 <template>
-  <p v-if="orderId">inside details id: {{ orderId }}</p>
+  <ul>
+    <li v-for="spec in specs" :key="spec.node.id">{{spec.node.id}} - {{spec.node.kw}} - {{spec.node.voltage}}</li>
+  </ul>
 </template>
 
 <style scoped>
