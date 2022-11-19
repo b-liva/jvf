0b813d735e6c4cd77a4a8ccf081b14f534f2f81d
b-liva <ebammo20@gmail.com>
2022-11-19 10:55:18 +0330

    Get cost by spec

--- src/components/cost/ProjectCosts.vue
+++ src/components/cost/ProjectCosts.vue
@@ -1,10 +1,22 @@
 <script setup>
 const props = defineProps(['specId'])
+import {useQuery} from "@vue/apollo-composable";
+import {computed, watch} from "vue";
+import {getCostsBySpec} from "../../graphql/cost/query/cost.graphql";
+
+const {result: costList, loading, error} = useQuery(getCostsBySpec,
+    () => ({
+      specId: props.specId
+    }))
+const costs = computed(() => costList.value?.getCostsBySpec.edges ?? [])
 
 </script>
 
 <template>
   <div>project cost list: {{ specId }}</div>
+  <ul>
+    <li v-for="cost in costs" :key="cost.node.id">{{ cost.node.id }} - {{ cost.node.chNumber }}</li>
+  </ul>
 </template>
 
 <style scoped>
