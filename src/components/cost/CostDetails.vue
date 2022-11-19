04b6e318be48dd1519acc71c4d3cd8b6f514ae8b
b-liva <ebammo20@gmail.com>
2022-11-19 11:04:52 +0330

    Implement cost details

---
+++ src/components/cost/CostDetails.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+cost details
+</template>
+
+<style scoped>
+
+</style>
--- src/components/cost/ProjectCosts.vue
+++ src/components/cost/ProjectCosts.vue
@@ -3,7 +3,7 @@ const props = defineProps(['specId'])
 import {useQuery} from "@vue/apollo-composable";
 import {computed, watch} from "vue";
 import {getCostsBySpec} from "../../graphql/cost/query/cost.graphql";
-
+import CostDetails from "./CostDetails.vue";
 const {result: costList, loading, error} = useQuery(getCostsBySpec,
     () => ({
       specId: props.specId
@@ -17,6 +17,9 @@ const costs = computed(() => costList.value?.getCostsBySpec.edges ?? [])
   <ul>
     <li v-for="cost in costs" :key="cost.node.id">{{ cost.node.id }} - {{ cost.node.chNumber }}</li>
   </ul>
+  <div>
+    <CostDetails/>
+  </div>
 </template>
 
 <style scoped>
