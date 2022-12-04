6c7e1dc567c981932cf88816dc6aaa4920efba30
liva <ebammo20@gmail.com>
2022-12-04 23:35:41 +0330

    use useLazyQuery to load proformas by order id

--- src/components/proforma/ProformasByOrder.vue
+++ src/components/proforma/ProformasByOrder.vue
@@ -1,22 +1,18 @@
 <script setup>
 import {useStore} from "../../store/store.js";
-import {useQuery} from "@vue/apollo-composable";
+import {useLazyQuery} from "@vue/apollo-composable";
 import {getProformasByOrderId} from "../../graphql/proforma/query/proforma.graphql";
 import {computed, watch} from "vue";
 
 const store = useStore();
 
 
-function getProformas(){
-  const {result: proformasByOrder, loading, errors} = useQuery(getProformasByOrderId,
-      () => ({
-        orderId: store.orderId,
-      }), {
-        prefetch: false
-      })
-  return{proformasByOrder, loading, errors}
-}
-const {proformasByOrder, loading, errors} = getProformas();
+const {result: proformasByOrder, loading, errors, load: getProformas} = useLazyQuery(getProformasByOrderId,
+    () => ({
+      orderId: store.orderId,
+    }), {
+      prefetch: false
+    })
 const proformas = computed(() => proformasByOrder.value?.getProformasByOrderId.edges ?? [])
 watch(
     () => store.orderId,
@@ -30,7 +26,7 @@ watch(
 <template>
   <ul>
     <li @click="store.proformaId = proforma.node.id" v-for="proforma in proformas" :key="proforma.node.id">
-      {{proforma.node.id}} - {{proforma.node.number}} - {{proforma.node.perm}}
+      {{ proforma.node.id }} - {{ proforma.node.number }} - {{ proforma.node.perm }}
     </li>
   </ul>
 </template>
