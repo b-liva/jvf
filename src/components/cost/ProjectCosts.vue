51da87d1ae7c6abe5da1ee4942aac6ea634c96a9
liva <ebammo20@gmail.com>
2022-12-04 23:47:57 +0330

    useLazyQuery in project costs component

--- src/components/cost/ProjectCosts.vue
+++ src/components/cost/ProjectCosts.vue
@@ -1,17 +1,21 @@
 <script setup>
 import {useStore} from "../../store/store.js";
-import {ref} from "vue";
-import {useQuery} from "@vue/apollo-composable";
+import {ref, watch} from "vue";
+import {useLazyQuery} from "@vue/apollo-composable";
 import {computed} from "vue";
 import {getCostsBySpec} from "../../graphql/cost/query/cost.graphql";
 import CostDetails from "./CostDetails.vue";
 
 const store = useStore();
-const {result: costList, loading, error} = useQuery(getCostsBySpec,
+const {result: costList, loading, error, load} = useLazyQuery(getCostsBySpec,
     () => ({
       specId: store.proformaSpecId
     }))
 const costs = computed(() => costList.value?.getCostsBySpec.edges ?? [])
+watch(
+    () => store.proformaSpecId,
+    () => load()
+)
 </script>
 
 <template>
