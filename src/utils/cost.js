567dec24c4bbcb17245fc115327078d76e78dafb
b-liva <ebammo20@gmail.com>
2022-12-06 16:17:20 +0330

    Implement Cost class

---
+++ src/utils/cost.js
@@ -0,0 +1,52 @@
+export default class Cost{
+  constructor(id, chNumber, dateFa, spec) {
+    this.id = id;
+    this.chNumber = chNumber;
+    this.dateFa = dateFa;
+    this.spec = spec;
+    this.checkValues();
+  }
+
+  replaceWith(value, replaceValue){
+    return value ? value : replaceValue;
+  }
+  checkValues(){
+    this.replaceWith(this.id, '');
+    this.replaceWith(this.chNumber, '');
+    this.replaceWith(this.dateFa, '');
+    this.replaceWith(this.spec.id, '');
+  }
+}
+
+class CostRow{
+  constructor(obj) {
+    for (const prop in obj){
+      if (obj.hasOwnProperty(prop)){
+        this[prop] = obj[prop];
+      }
+    }
+  }
+  getProps(){
+    return Object.getOwnPropertyNames(this)
+  }
+  replaceWith(value, replaceValue){
+    return value ? value : replaceValue;
+  }
+  checkValues(){
+    this.getProps().forEach(pr => {
+      this.replaceWith(pr.id, '');
+      this.replaceWith(pr.qty, 0);
+      this.replaceWith(pr.price, 0);
+    })
+  }
+}
+
+class CostRowSet extends CostRow{
+  checkValues() {
+    this.getProps().forEach(pr => {
+      this.replaceWith(pr.id, '');
+      this.replaceWith(pr.qty, 0);
+      this.replaceWith(pr.price, 0);
+    })
+  }
+}
