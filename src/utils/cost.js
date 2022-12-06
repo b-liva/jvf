7137dd480b28d3e788256256c39d2db6de6adff2
liva <ebammo20@gmail.com>
2022-12-06 23:38:24 +0330

    Refactor prop creation

--- src/utils/cost.js
+++ src/utils/cost.js
@@ -1,121 +1,119 @@
-export default class Cost{
-  constructor(id, cost) {
-    this.id = id;
-    this.chNumber = cost.chNumber;
-    this.dateFa = cost.dateFa;
-    this.spec = cost.spec;
-    this.checkValues();
-
-    this.costRows.forEach(item => {
-      this[item] = new CostRow(cost[item]);
-    })
-    this.dependents.forEach(item => {
-      this[item] = new DependentCost(cost[item]);
-    })
-    this.bearingcostSet = new BearingCostSet(cost['bearingcostSet']);
-    this.testcostSet = new BearingCostSet(cost['testcostSet']);
-    this.certificatecostSet = new BearingCostSet(cost['certificatecostSet']);
-  }
-
-  costRows = [
-    'aluingot',
-    'castiron',
-    'curotor',
-    'custator',
-    'insulation',
-    'other',
-    'overheadcost',
-    'siliconsheet',
-    'steel',
-    'steelrebar',
-    'wagecost',
-  ]
-  costRowSets = [
-    'bearingcostSet',
-    'testcostSet',
-    'certificatecostSet',
-  ]
-  dependents = [
-    'standardparts',
-    'generalcost',
-  ]
-
-  replaceWith(value, replaceValue){
-    return value ? value : replaceValue;
-  }
-  checkValues(){
-    this.replaceWith(this.id, '');
-    this.replaceWith(this.chNumber, '');
-    this.replaceWith(this.dateFa, '');
-    this.replaceWith(this.spec.id, '');
-  }
-}
-
-class CostRow{
-  constructor(obj) {
-    for (const prop in obj){
-      if (obj.hasOwnProperty(prop)){
-        this[prop] = obj[prop];
-      }
+export default class Cost {
+    constructor(id, cost) {
+        this.id = id;
+        this.chNumber = cost.chNumber;
+        this.dateFa = cost.dateFa;
+        this.spec = cost.spec;
+        this.checkValues();
+        this.createCostRows(cost);
     }
-  }
-  getProps(){
-    return Object.getOwnPropertyNames(this)
-  }
-  replaceWith(value, replaceValue){
-    return value ? value : replaceValue;
-  }
-  checkValues(){
-    this.getProps().forEach(pr => {
-      this.replaceWith(pr.id, '');
-      this.replaceWith(pr.qty, 0);
-      this.replaceWith(pr.price, 0);
-    })
-  }
+
+    costRows = [
+        { name: 'aluingot', factoryClass: CostRow },
+        { name: 'castiron', factoryClass: CostRow },
+        { name: 'curotor', factoryClass: CostRow },
+        { name: 'custator', factoryClass: CostRow },
+        { name: 'insulation', factoryClass: CostRow },
+        { name: 'other', factoryClass: CostRow },
+        { name: 'overheadcost', factoryClass: CostRow },
+        { name: 'siliconsheet', factoryClass: CostRow },
+        { name: 'steel', factoryClass: CostRow },
+        { name: 'steelrebar', factoryClass: CostRow },
+        { name: 'wagecost', factoryClass: CostRow },
+        { name: 'bearingcostSet', factoryClass: BearingCostSet },
+        { name: 'testcostSet', factoryClass: TestCostSet },
+        { name: 'certificatecostSet', factoryClass: CertificateCostSet },
+        { name: 'standardparts', factoryClass: DependentCost },
+        { name: 'generalcost', factoryClass: DependentCost },
+    ]
+
+    createCostRows(cost) {
+        this.costRows.forEach(item => {
+            this[item.name] = new item.factoryClass(cost[item.name]);
+        })
+    }
+
+    replaceWith(value, replaceValue) {
+        return value ? value : replaceValue;
+    }
+
+    checkValues() {
+        this.replaceWith(this.id, '');
+        this.replaceWith(this.chNumber, '');
+        this.replaceWith(this.dateFa, '');
+        this.replaceWith(this.spec.id, '');
+    }
 }
 
-class CostRowSet extends CostRow{
-  // .edges => []
-  checkValues() {
-    this.getProps().forEach(pr => {
-      this.replaceWith(pr.node.id, '');
-      this.replaceWith(pr.node.qty, 0);
-      this.replaceWith(pr.node.price, 0);
-      this.checkMore(pr);
-    })
-  }
-  checkMore(pr){
+class CostRow {
+    constructor(obj) {
+        for (const prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                this[prop] = obj[prop];
+            }
+        }
+    }
 
-  }
+    getProps() {
+        return Object.getOwnPropertyNames(this)
+    }
+
+    replaceWith(value, replaceValue) {
+        return value ? value : replaceValue;
+    }
+
+    checkValues() {
+        this.getProps().forEach(pr => {
+            this.replaceWith(pr.id, '');
+            this.replaceWith(pr.qty, 0);
+            this.replaceWith(pr.price, 0);
+        })
+    }
 }
 
-class BearingCostSet extends CostRowSet{
-  checkMore(brc) {
-    this.replaceWith(brc.node.bearing.id, '');
-    this.replaceWith(brc.node.bearing.name, '');
-  }
+class CostRowSet extends CostRow {
+    // .edges => []
+    checkValues() {
+        this.getProps().forEach(pr => {
+            this.replaceWith(pr.node.id, '');
+            this.replaceWith(pr.node.qty, 0);
+            this.replaceWith(pr.node.price, 0);
+            this.checkMore(pr);
+        })
+    }
+
+    checkMore(pr) {
+
+    }
 }
 
-class TestCostSet extends CostRowSet{
-  checkMore(tst) {
-    this.replaceWith(tst.node.test.id, '');
-    this.replaceWith(tst.node.test.name, '');
-  }
+class BearingCostSet extends CostRowSet {
+    checkMore(brc) {
+        this.replaceWith(brc.node.bearing.id, '');
+        this.replaceWith(brc.node.bearing.name, '');
+    }
 }
 
-class CertificateCostSet extends CostRowSet{
-  checkMore(crt) {
-    this.replaceWith(crt.node.certificate.id, '');
-    this.replaceWith(crt.node.certificate.name, '');
-  }
+class TestCostSet extends CostRowSet {
+    checkMore(tst) {
+        this.replaceWith(tst.node.test.id, '');
+        this.replaceWith(tst.node.test.name, '');
+    }
 }
 
-class DependentCost extends CostRow{
-  checkValues(){
-    this.getProps().forEach(pr => {
-      this.replaceWith(pr.id, '');
-      this.replaceWith(pr.amount, 0);
-      this.replaceWith(pr.percent, 0);
-    })
-  }
+class CertificateCostSet extends CostRowSet {
+    checkMore(crt) {
+        this.replaceWith(crt.node.certificate.id, '');
+        this.replaceWith(crt.node.certificate.name, '');
+    }
+}
+
+class DependentCost extends CostRow {
+    checkValues() {
+        this.getProps().forEach(pr => {
+            this.replaceWith(pr.id, '');
+            this.replaceWith(pr.amount, 0);
+            this.replaceWith(pr.percent, 0);
+        })
+    }
 }
