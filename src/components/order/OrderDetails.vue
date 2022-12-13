843513d8727c53533fb20ac4821aed6f1bbd3d85
b-liva <ebammo20@gmail.com>
2022-12-13 15:43:57 +0330

    Show order specs as table

--- src/components/order/OrderDetails.vue
+++ src/components/order/OrderDetails.vue
@@ -22,9 +22,47 @@ watch(
 </script>
 
 <template>
-  <ul>
-    <li v-for="spec in specs" :key="spec.node.id">{{ spec.node.id }} - {{ spec.node.kw }} - {{ spec.node.voltage }}</li>
-  </ul>
+  <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" class="py-3 px-6">
+          تعداد
+        </th>
+        <th scope="col" class="py-3 px-6">
+          کیلووات
+        </th>
+        <th scope="col" class="py-3 px-6">
+          سرعت
+        </th>
+        <th scope="col" class="py-3 px-6">
+          ولتاژ
+        </th>
+        <th scope="col" class="py-3 px-6">
+
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr
+          v-for="spec in specs" :key="spec.node.id"
+          class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+        <td class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          {{spec.node.qty}}
+        </td>
+        <td class="py-4 px-6">
+          {{spec.node.kw}}
+        </td>
+        <td class="py-4 px-6">
+          {{spec.node.rpmNew.rpm}}
+        </td>
+        <td class="py-4 px-6">
+          {{spec.node.voltage}}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
