5e48357f23e5f749ad2728cbf9702808f26e050d
b-liva <ebammo20@gmail.com>
2022-12-18 11:49:00 +0330

    Style proforma list

--- src/components/proforma/ProformasByOrder.vue
+++ src/components/proforma/ProformasByOrder.vue
@@ -27,15 +27,39 @@ function selectedProformaClass(id){
 </script>
 
 <template>
-  <h3 class="mb-4 text-green-500 font-semibold">پیش فاکتور</h3>
-  <dl class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700">
-    <div
-        @click="store.proformaId = proforma.node.id" v-for="proforma in proformas" :key="proforma.node.id"
-        class="flex flex-col pb-3">
-      <dt class="mb-1 text-gray-500 md:text-lg dark:text-gray-400">{{proforma.node.dateFa}}</dt>
-      <dd :class="selectedProformaClass(proforma.node.id)">{{proforma.node.number}}</dd>
+  <div class="flex flex-col mt-8">
+    <div class="overflow-x-auto rounded-lg">
+      <div class="align-middle inline-block min-w-full">
+        <div class="shadow overflow-hidden sm:rounded-lg">
+          <h3 class="mb-4 text-green-500 font-semibold">پیش فاکتور</h3>
+          <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+            <tr>
+              <th scope="col" class="p-4 text-right text-xs font-medium text-gray-500 tracking-wider">مشتری</th>
+              <th scope="col" class="p-4 text-right text-xs font-medium text-gray-500 tracking-wider">تاریخ</th>
+            </tr>
+            </thead>
+            <tbody class="bg-white">
+            <tr
+                @click="store.proformaId = proforma.node.id"
+                v-for="(proforma, index) in proformas"
+                :key="proforma.node.id"
+                :class="{'bg-gray-50': index % 2 === 1}">
+              <td
+                  :class="{
+                'rounded-lg rounded-right': index % 2 === 1,
+                'text-lg font-semibold text-blue-500': proforma.node.id === store.proformaId
+                }"
+                  class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">{{proforma.node.number}}</td>
+              <td
+                  class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">{{proforma.node.dateFa}}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  </dl>
+  </div>
 </template>
 
 <style scoped>
