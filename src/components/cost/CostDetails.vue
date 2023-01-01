e5a59f3a4d39d004b92f68f4f6bac5db2a30254b
liva <ebammo20@gmail.com>
2023-01-01 13:28:02 +0330

    Remove unused reset

--- src/components/cost/CostDetails.vue
+++ src/components/cost/CostDetails.vue
@@ -1,15 +1,13 @@
 <script setup>
 import {useStore} from "../../store/store.js";
-import {computed, ref, watch} from "vue";
 import Cost from "../../utils/cost.js";
-import Reset from "../../utils/reset.js";
 import {useLazyQuery} from "@vue/apollo-composable";
 import {getProjectCostDetails} from "../../graphql/cost/query/cost.graphql";
 import ProjectCostTable from '../cost/ProjectCostTable.vue';
+import {ref, watch} from "vue";
 let show = ref(false)
 const store = useStore();
 store.cost = Cost.reset();
-const reset = new Reset();
 
 const {result: projectCostDetailsResult, loading, load: load, onResult: onResult} = useLazyQuery(
     getProjectCostDetails,
@@ -18,7 +16,7 @@ const {result: projectCostDetailsResult, loading, load: load, onResult: onResult
 );
 
 onResult(qr => {
-  const costRaw = reset.getValueOrReset(getValue)
+  const costRaw = projectCostDetailsResult.value?.getProjectCostDetails ?? Cost.reset()
   const costObj = new Cost(costRaw.id, costRaw);
   store.cost = Cost.copy(costObj);
 })
@@ -30,18 +28,11 @@ watch(
 watch(
     () => store.costId,
     () => {
-      reset.resetting.value = false;
       show.value = true;
       load()
     },
     {deep: true}
 )
-function getValue(){
-  return{
-    value: projectCostDetailsResult.value?.getProjectCostDetails ?? Cost.reset(),
-    resetValue: Cost.reset()
-  }
-}
 </script>
 
 <template>
