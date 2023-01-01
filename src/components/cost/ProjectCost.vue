a264b85c60e52b6fe913db5daa1a6b6015c3aa3b
liva <ebammo20@gmail.com>
2023-01-01 14:44:59 +0330

    Add backend template url

--- src/components/cost/ProjectCost.vue
+++ src/components/cost/ProjectCost.vue
@@ -6,6 +6,7 @@ import ProformasByOrder from '../proforma/ProformasByOrder.vue';
 import ProformaDetails from '../proforma/ProformaDetails.vue';
 import ProjectCosts from '../cost/ProjectCosts.vue';
 import CostDetails from '../cost/CostDetails.vue';
+import {baseUrl} from "../../utils/methods";
 
 const store = useStore();
 
@@ -34,23 +35,6 @@ function idIsNull(id){
           <div class="flex-1 px-3 bg-white divide-y space-y-1">
             <ul class="space-y-2 pb-2">
               <li>
-                <form action="#" method="GET" class="lg:hidden">
-                  <label for="mobile-search" class="sr-only">Search</label>
-                  <div class="relative">
-                    <div class="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <svg class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg">
-                        <path
-                            d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                      </svg>
-                    </div>
-                    <input type="text" name="email" id="mobile-search"
-                           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-cyan-600 focus:ring-cyan-600 block w-full pl-10 p-2.5"
-                           placeholder="Search">
-                  </div>
-                </form>
-              </li>
-              <li>
                 <a href="#"
                    class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                   <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
@@ -60,6 +44,18 @@ function idIsNull(id){
                           clip-rule="evenodd"></path>
                   </svg>
                   <span class="ml-3 flex-1 whitespace-nowrap">بهای تمام شده</span>
+                </a>
+              </li>
+              <li>
+                <a :href="baseUrl() + '/cdash'"
+                   class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                  <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                       fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                          d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                          clip-rule="evenodd"></path>
+                  </svg>
+                  <span class="ml-3 flex-1 whitespace-nowrap">بازگشت</span>
                 </a>
               </li>
             </ul>
