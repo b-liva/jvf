5f9dc539aa48e1b69389ce6742dd5307868d2db2
liva <ebammo20@gmail.com>
2023-01-02 15:20:36 +0330

    Create cost path

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomePage from "../components/test/HomePage.vue";
 import AboutPage from "../components/test/AboutPage.vue";
+import ProjectCost from '../components/cost/ProjectCost.vue';
 
 const routes = [
     {path: '/', name: 'home', component: HomePage},
-    {path: '/about', name: 'about', component: AboutPage}
+    {path: '/about', name: 'about', component: AboutPage},
+    {path: '/cost', name: 'cost', component: ProjectCost}
 ]
 
 const router = createRouter({
