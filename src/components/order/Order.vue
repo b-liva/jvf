fdde5df52f4d0f3be5930e213d6974adf51f12c5
liva <ebammo20@gmail.com>
2023-01-02 16:50:28 +0330

    Create Order component

---
+++ src/components/order/Order.vue
@@ -0,0 +1,15 @@
+<script setup>
+import OrdersByNumber from "./OrdersByNumber.vue"
+</script>
+
+<template>
+  <div class="grid grid-cols-4 text-xs">
+    <div class=""><OrdersByNumber/></div>
+    <div class=""></div>
+    <div class=""></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
