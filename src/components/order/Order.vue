895e59f5aebea6ec5fb39f9577d81b0419ec8106
liva <ebammo20@gmail.com>
2023-01-02 17:28:25 +0330

    Create Order.vue

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,12 +1,12 @@
-<script setup>
-import OrdersByNumber from "./OrdersByNumber.vue"
-</script>
-
 <template>
-  <div class="grid grid-cols-4 gap-4">
-    <div class=""><OrdersByNumber/></div>
-  </div>
+
 </template>
+
+<script>
+export default {
+  name: "Order"
+}
+</script>
 
 <style scoped>
 
