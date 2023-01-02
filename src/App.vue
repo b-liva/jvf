697b095a932e00bfb1005c2516bd38f0dbb2e41d
liva <ebammo20@gmail.com>
2023-01-02 16:33:14 +0330

    Render routes dynamically and Add order route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {baseUrl} from "./utils/methods";
+import router from "./router/routes";
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import {baseUrl} from "./utils/methods";
         <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
           <div class="flex-1 px-3 bg-white divide-y space-y-1">
             <ul class="space-y-2 pb-2">
-              <RouterLink to="/">
+              <RouterLink v-for="route in router.getRoutes()" :to="route.path">
                 <li>
                   <a href="#"
                      class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
@@ -21,21 +22,7 @@ import {baseUrl} from "./utils/methods";
                             d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">نخست</span>
-                  </a>
-                </li>
-              </RouterLink>
-              <RouterLink to="/cost">
-                <li>
-                  <a :href="baseUrl() + '/cdash'"
-                     class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd"
-                            d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">بهای تمام شده</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">{{route.props.default.title}}</span>
                   </a>
                 </li>
               </RouterLink>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,9 @@ import HomePage from "../components/test/HomePage.vue";
 import ProjectCost from '../components/cost/ProjectCost.vue';
 
 const routes = [
-    {path: '/', name: 'home', component: HomePage},
-    {path: '/cost', name: 'cost', component: ProjectCost}
+    {path: '/', name: 'home', component: HomePage, props:{title:'نخست'}},
+    {path: '/order', name: 'order', component: ProjectCost, props: {title: 'سفارش فروش'}},
+    {path: '/cost', name: 'cost', component: ProjectCost, props: {title: 'بهای تمام شده'}},
 ]
 
 const router = createRouter({
