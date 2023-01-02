18cdd1cda2ef6f03bcf2dd9baf7c2117ba085ffd
liva <ebammo20@gmail.com>
2023-01-02 17:28:06 +0330

    Rename Order to Orders.vue

---
+++ src/components/order/Orders.vue
@@ -0,0 +1,13 @@
+<script setup>
+import OrdersByNumber from "./OrdersByNumber.vue"
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-4">
+    <div class=""><OrdersByNumber/></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,11 +1,11 @@
 import {createRouter, createWebHistory} from "vue-router";
 import HomePage from "../components/test/HomePage.vue";
-import Order from "../components/order/Order.vue";
+import Orders from "../components/order/Orders.vue";
 import ProjectCost from '../components/cost/ProjectCost.vue';
 
 const routes = [
     {path: '/', name: 'home', component: HomePage, props:{title:'نخست'}},
-    {path: '/order', name: 'order', component: Order, props: {title: 'سفارش فروش'}},
+    {path: '/orders', name: 'orders', component: Orders, props: {title: 'سفارش فروش'}},
     {path: '/cost', name: 'cost', component: ProjectCost, props: {title: 'بهای تمام شده'}},
 ]
 
