058568e7e0e9c8e105937d3a59c131f39fb57ef2
liva <ebammo20@gmail.com>
2023-01-03 11:45:22 +0330

    Add customer to order filter

--- src/components/order/Filters.vue
+++ src/components/order/Filters.vue
@@ -7,15 +7,17 @@ import {computed, watch} from "vue";
 
 const orderStore = useOrderStore();
 let orderNumber = ref('');
+let customerName = ref('');
 const {result: orders, loading, error, load} = useLazyQuery(filterOrders,
     () => {
       return {
-        number: orderNumber.value
+        number: orderNumber.value,
+        customerName: customerName.value
       }
     }
 )
 watch(
-    () => orderNumber.value,
+    () => [orderNumber.value, customerName.value],
     () => {
       load()
     }
@@ -33,6 +35,15 @@ orderStore.orders = computed(() => orders.value?.filterOrders.edges ?? [])
         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
         v-model="orderNumber"
         required>
+    <span class="text-base font-normal text-gray-500">مشتری</span>
+    <input
+        type="text"
+        id="customer_name"
+        placeholder="مشتری"
+        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+        v-model="customerName"
+        required>
+    <div v-if="loading">Is Loading...</div>
   </div>
 </template>
 
