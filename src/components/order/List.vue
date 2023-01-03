a0e7062b76493e8a62a823f1869b697b4c221a3a
liva <ebammo20@gmail.com>
2023-01-03 17:40:36 +0330

    Add styles to order search results

--- src/components/order/List.vue
+++ src/components/order/List.vue
@@ -7,28 +7,28 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="overflow-x-auto rounded-lg px-4">
+  <div class="overflow-x-auto">
     <div
         v-for="(order, index) in orders"
         :key="order.node.id"
         @click="router.push({name: 'order', params:{id:order.node.id}})"
-        class="bg-white">
-      <div class="border rounded mb-6">
+        class="border bg-white rounded-lg p-2 mb-6">
+      <div class="">
         <div class="grid grid-cols-12">
-          <div class="p-1.5">{{ order.node.number }}</div>
-          <div class="col-start-13 text-green-700">{{order.node.owner.lastName}}</div>
+          <div class="text-base text-blue-500 p-1.5">{{ order.node.number }}</div>
+          <div class="col-start-13 text-green-700 p-1">{{order.node.owner.lastName}}</div>
         </div>
-        <div class="grid grid-cols-6 py-6">
+        <div class="grid grid-cols-6 py-4">
           <div class="col-span-3">{{ order.node.customer.name }}</div>
           <div class="">
-            <div>تعداد دستگاه</div>
-            <div>{{ order.node.totalQty }}</div>
+            <div class="text-center">تعداد دستگاه</div>
+            <div class=" text-center text-green-500">{{ order.node.totalQty }}</div>
           </div>
           <div class="">
-            <div>کیلووات</div>
-            <div>{{ order.node.totalKw }}</div>
+            <div class="text-center">کیلووات</div>
+            <div class="text-center text-green-500">{{ order.node.totalKw }}</div>
           </div>
-          <div class="">{{ order.node.dateFa }}</div>
+          <div class="text-center">{{ order.node.dateFa }}</div>
         </div>
       </div>
     </div>
