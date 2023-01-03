2ec8c9731c63ca98824ec44aff4e60ca5ba19ee8
liva <ebammo20@gmail.com>
2023-01-03 17:06:41 +0330

    Move menu to top

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,20 @@
 <script setup>
 import {baseUrl} from "./utils/methods";
 import {menuRoutes} from "./router/routes";
+import TopMenu from "./components/menu/TopMenu.vue";
 
 </script>
 
 <template>
   <div class="">
-    <div class="">header</div>
+    <TopMenu/>
 <!--    <aside href="" class="fixed hidden z-20 h-full top-0 right-0 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"></aside>-->
     <div class="grid grid-cols-4">
       <aside id="sidebar" class="col-span-1" aria-label="Sidebar">
         <div class="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
           <div class="flex-1 flex flex-col overflow-y-auto">
             <div class="flex-1 px-3 bg-gray-100 divide-y space-y-1">
-              <ul class="space-y-2 pb-2">
-                <RouterLink v-for="route in menuRoutes" :to="route.path">
-                  <li>
-                    <a href="#"
-                       class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                      <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-                           fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                              d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                              clip-rule="evenodd"></path>
-                      </svg>
-                      <span class="ml-3 flex-1 whitespace-nowrap">{{route.props.title}}</span>
-                    </a>
-                  </li>
-                </RouterLink>
-                <li>
-                  <a :href="baseUrl() + '/cdash'"
-                     class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                    <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-                         fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd"
-                            d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">پنل قدیمی</span>
-                  </a>
-                </li>
-              </ul>
+              search box
             </div>
           </div>
         </div>
---
+++ src/components/menu/TopMenu.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {baseUrl} from "../../utils/methods";
+import {menuRoutes} from "../../router/routes";
+import {computed} from "vue";
+const menuCount = computed(() => menuRoutes.length < 6 ? 6 : menuRoutes.length)
+console.log(menuCount)
+</script>
+
+<template>
+  <div class="grid text-center" :class="`grid-cols-${menuCount}`">
+    <RouterLink v-for="route in menuRoutes" :to="route.path">
+      <span class="">{{ route.props.title }}</span>
+    </RouterLink>
+    <a :href="baseUrl() + '/cdash'" :class="`col-start-${menuCount + 1}`">
+      <span class="">پنل قدیمی</span>
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
