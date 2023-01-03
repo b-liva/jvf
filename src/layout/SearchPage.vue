aff053944d2e743ea0c0b2cf33d8aae4ea568014
liva <ebammo20@gmail.com>
2023-01-04 00:59:45 +0330

    Make orders component fills height

--- src/layout/SearchPage.vue
+++ src/layout/SearchPage.vue
@@ -3,11 +3,11 @@
 </script>
 
 <template>
-  <div class="grid grid-cols-5">
-    <aside id="sidebar" class="col-span-1" aria-label="Sidebar">
-      <div class="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
+  <div class="grid grid-cols-5 h-full">
+    <aside id="sidebar" class="col-span-1 h-full bg-gray-100" aria-label="Sidebar">
+      <div class="relative flex-1 flex flex-col min-h-0 border-r border-gray-200  pt-0">
         <div class="flex-1 flex flex-col overflow-y-auto">
-          <div class="flex-1 px-3 bg-gray-100 divide-y space-y-1">
+          <div class="flex-1 px-3 divide-y space-y-1">
             <slot name="searchFilters"></slot>
           </div>
         </div>
