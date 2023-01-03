acb5435ab6265da29a69592fef7c21cf89548c9b
liva <ebammo20@gmail.com>
2023-01-03 14:52:46 +0330

    Wrap filter fields in div tag

--- src/components/order/Filters.vue
+++ src/components/order/Filters.vue
@@ -36,22 +36,22 @@ function handleNullFilterInputs(){
 
 <template>
   <div>
-    <span class="text-base font-normal text-gray-500">شماره درخواست</span>
-    <input
-        type="number"
-        id="order_number"
-        placeholder="شماره درخواست"
-        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-        v-model="orderNumber"
-        required>
-    <span class="text-base font-normal text-gray-500">مشتری</span>
-    <input
-        type="text"
-        id="customer_name"
-        placeholder="مشتری"
-        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
-        v-model="customerName"
-        required>
+    <div class=""><span class="text-xs font-normal text-gray-500">شماره درخواست</span>
+      <input
+          type="number"
+          id="order_number"
+          placeholder="شماره درخواست"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+          v-model="orderNumber"
+          required></div>
+    <div class="mt-3"><span class="text-xs font-normal text-gray-500">مشتری</span>
+      <input
+          type="text"
+          id="customer_name"
+          placeholder="مشتری"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"
+          v-model="customerName"
+          required></div>
     <div v-if="loading">Is Loading...</div>
   </div>
 </template>
