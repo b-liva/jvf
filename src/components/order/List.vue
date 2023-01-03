51c56618454da9594add234e81dffe5a040423c2
liva <ebammo20@gmail.com>
2023-01-03 14:59:08 +0330

    Remove unused nested tags

--- src/components/order/List.vue
+++ src/components/order/List.vue
@@ -6,20 +6,14 @@ const router = useRouter();
 </script>
 
 <template>
-  <div class="">
-    <div class="overflow-x-auto rounded-lg">
-      <div class="">
-        <div class="">
-          <h3 class="mb-4 text-green-500 font-semibold">درخواست</h3>
-          <div
-              v-for="(order, index) in orders"
-              :key="order.node.id"
-              @click="router.push({name: 'order', params:{id:order.node.id}})"
-              :class="{'bg-gray-50': index % 2 === 1}">
-            {{ order.node.customer.name }}
-          </div>
-        </div>
-      </div>
+  <div class="overflow-x-auto rounded-lg">
+    <h3 class="mb-4 text-green-500 font-semibold">درخواست</h3>
+    <div
+        v-for="(order, index) in orders"
+        :key="order.node.id"
+        @click="router.push({name: 'order', params:{id:order.node.id}})"
+        :class="{'bg-gray-50': index % 2 === 1}">
+      {{ order.node.customer.name }}
     </div>
   </div>
 </template>
