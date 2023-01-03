977158038640bbb6fbca61c19c1abbcfcc83280d
liva <ebammo20@gmail.com>
2023-01-03 16:24:16 +0330

    Rearrange layouts

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,31 @@ import {menuRoutes} from "./router/routes";
 </script>
 
 <template>
-  <div class="flex overflow-hidden bg-white pt-16">
-    <aside id="sidebar"
-           class="fixed hidden z-20 h-full top-0 right-0 pt-16 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"
-           aria-label="Sidebar">
-      <div class="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
-        <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
-          <div class="flex-1 px-3 bg-white divide-y space-y-1">
-            <ul class="space-y-2 pb-2">
-              <RouterLink v-for="route in menuRoutes" :to="route.path">
+  <div class="">
+    <div class="">header</div>
+<!--    <aside href="" class="fixed hidden z-20 h-full top-0 right-0 flex lg:flex flex-shrink-0 flex-col w-64 transition-width duration-75"></aside>-->
+    <div class="grid grid-cols-4">
+      <aside id="sidebar" class="col-span-1" aria-label="Sidebar">
+        <div class="relative flex-1 flex flex-col min-h-0 border-r border-gray-200 bg-white pt-0">
+          <div class="flex-1 flex flex-col overflow-y-auto">
+            <div class="flex-1 px-3 bg-gray-100 divide-y space-y-1">
+              <ul class="space-y-2 pb-2">
+                <RouterLink v-for="route in menuRoutes" :to="route.path">
+                  <li>
+                    <a href="#"
+                       class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                      <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
+                           fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                              d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                              clip-rule="evenodd"></path>
+                      </svg>
+                      <span class="ml-3 flex-1 whitespace-nowrap">{{route.props.title}}</span>
+                    </a>
+                  </li>
+                </RouterLink>
                 <li>
-                  <a href="#"
+                  <a :href="baseUrl() + '/cdash'"
                      class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                     <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
                          fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -23,31 +37,19 @@ import {menuRoutes} from "./router/routes";
                             d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
                             clip-rule="evenodd"></path>
                     </svg>
-                    <span class="ml-3 flex-1 whitespace-nowrap">{{route.props.title}}</span>
+                    <span class="ml-3 flex-1 whitespace-nowrap">پنل قدیمی</span>
                   </a>
                 </li>
-              </RouterLink>
-              <li>
-                <a :href="baseUrl() + '/cdash'"
-                   class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
-                  <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75"
-                       fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                          d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
-                          clip-rule="evenodd"></path>
-                  </svg>
-                  <span class="ml-3 flex-1 whitespace-nowrap">پنل قدیمی</span>
-                </a>
-              </li>
-            </ul>
+              </ul>
+            </div>
           </div>
         </div>
+      </aside>
+      <div id="main-content" class="col-span-3 h-full w-full bg-gray-100 relative overflow-y-auto">
+        <main>
+          <RouterView />
+        </main>
       </div>
-    </aside>
-    <div id="main-content" class="h-full w-full bg-gray-50 relative overflow-y-auto lg:mr-64">
-      <main>
-        <RouterView />
-      </main>
     </div>
   </div>
 </template>
--- src/components/order/List.vue
+++ src/components/order/List.vue
@@ -1,19 +1,33 @@
 <script setup>
 import {useRouter} from "vue-router";
+
 const props = defineProps(['orders'])
 const router = useRouter();
 
 </script>
 
 <template>
-  <div class="overflow-x-auto rounded-lg">
-    <h3 class="mb-4 text-green-500 font-semibold">درخواست</h3>
+  <div class="overflow-x-auto rounded-lg px-4">
     <div
         v-for="(order, index) in orders"
         :key="order.node.id"
         @click="router.push({name: 'order', params:{id:order.node.id}})"
-        :class="{'bg-gray-50': index % 2 === 1}">
-      {{ order.node.customer.name }}
+        class="bg-white">
+      <div class="border rounded mb-6">
+        <div class="p-1.5">{{ order.node.number }}</div>
+        <div class="grid grid-cols-6 py-6">
+          <div class="col-span-3">{{ order.node.customer.name }}</div>
+          <div class="">
+            <div>تعداد دستگاه</div>
+            <div>{{ order.node.totalQty }}</div>
+          </div>
+          <div class="">
+            <div>کیلووات</div>
+            <div>{{ order.node.totalKw }}</div>
+          </div>
+          <div class="">{{ order.node.dateFa }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
