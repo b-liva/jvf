f6261c159fcb2d8c2d16b6d158b9d2ed19b68bd9
liva <ebammo20@gmail.com>
2023-01-03 17:31:37 +0330

    Add owner to order list

--- src/components/order/List.vue
+++ src/components/order/List.vue
@@ -14,7 +14,10 @@ const router = useRouter();
         @click="router.push({name: 'order', params:{id:order.node.id}})"
         class="bg-white">
       <div class="border rounded mb-6">
-        <div class="p-1.5">{{ order.node.number }}</div>
+        <div class="grid grid-cols-12">
+          <div class="p-1.5">{{ order.node.number }}</div>
+          <div class="col-start-13 text-green-700">{{order.node.owner.lastName}}</div>
+        </div>
         <div class="grid grid-cols-6 py-6">
           <div class="col-span-3">{{ order.node.customer.name }}</div>
           <div class="">
