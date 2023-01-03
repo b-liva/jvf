2f5f1bb7b630e828c2ecf47e011bec196822669b
liva <ebammo20@gmail.com>
2023-01-03 13:57:30 +0330

    Change Orders vue layout

--- src/components/order/Orders.vue
+++ src/components/order/Orders.vue
@@ -7,9 +7,9 @@ const orderStore = useOrderStore()
 </script>
 
 <template>
-  <div class="">
-    <Filters/>
-    <List :orders="orderStore.orders"/>
+  <div class="grid grid-cols-4 gap-8 p-6">
+    <Filters class="col-span-1 pt-12"/>
+    <List :orders="orderStore.orders" class="col-span-3 pt-12"/>
   </div>
 </template>
 
