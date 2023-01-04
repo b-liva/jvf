f032a451c677973e8c221f09545d59f2a6cee5c8
liva <ebammo20@gmail.com>
2023-01-04 13:01:19 +0330

    Add some style to header

--- src/components/menu/TopMenu.vue
+++ src/components/menu/TopMenu.vue
@@ -2,16 +2,18 @@
 import {baseUrl} from "../../utils/methods";
 import {menuRoutes} from "../../router/routes";
 import {computed} from "vue";
-const menuCount = computed(() => menuRoutes.length < 6 ? 6 : menuRoutes.length)
-console.log(menuCount)
+const menuCount = computed(() => menuRoutes.length < 9 ? 9 : menuRoutes.length)
 </script>
 
 <template>
-  <div class="grid text-center" :class="`grid-cols-${menuCount}`">
-    <RouterLink v-for="route in menuRoutes" :to="route.path">
-      <span class="">{{ route.props.title }}</span>
+  <div class="grid text-center py-4 shadow-xl" :class="`grid-cols-${menuCount}`">
+    <RouterLink v-for="route in menuRoutes" :to="route.path" class="border-l py-2">
+      <span class="text-gray-500">{{ route.props.title }}</span>
     </RouterLink>
-    <a :href="baseUrl() + '/cdash'" :class="`col-start-${menuCount + 1}`">
+    <a
+        :href="baseUrl() + '/cdash'"
+        class="text-gray-500 py-2 pl-2"
+        :class="`col-start-${menuCount + 1}`">
       <span class="">پنل قدیمی</span>
     </a>
   </div>
