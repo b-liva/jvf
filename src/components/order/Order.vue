a416dd6a93b4e4c5cf1d0aa55f1c30c7728ed12e
liva <ebammo20@gmail.com>
2023-01-05 00:09:29 +0330

    Add code to spec table

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -50,6 +50,7 @@ const props = [
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" class="px-6 py-3">ردیف</th>
+            <th scope="col" class="px-6 py-3">کد</th>
             <th scope="col" class="px-6 py-3">کیلووات </th>
             <th scope="col" class="px-6 py-3">دور</th>
             <th scope="col" class="px-6 py-3">ولتاژ</th>
@@ -64,6 +65,7 @@ const props = [
 
           <tr v-for="(row, index) in [1,2,3,4,5]" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{index}}</th>
+            <td class="px-6 py-4">1010025</td>
             <td class="px-6 py-4">110</td>
             <td class="px-6 py-4">1500</td>
             <td class="px-6 py-4">380</td>
