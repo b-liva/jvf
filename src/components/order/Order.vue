af5517385b42d833c7c0560bce447ddfcf679afb
liva <ebammo20@gmail.com>
2023-01-04 13:22:44 +0330

    Split order to 3 columns

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -4,7 +4,12 @@ const route = useRoute();
 </script>
 
 <template>
-<div>{{route.params.id}}</div>
+<div class="grid grid-cols-9 mt-6">
+  <div class="col-span-2 bg-blue-100">01</div>
+  <div class="col-span-5 bg-blue-200">02</div>
+  <div class="col-span-2 bg-blue-300">03</div>
+  {{route.params.id}}
+</div>
 </template>
 
 <style scoped>
