60b4f6af53e2af4ec142a32c2749d49309dfd216
liva <ebammo20@gmail.com>
2023-01-05 16:50:25 +0330

    Add base customer details page layout

--- src/components/customer/Customer.vue
+++ src/components/customer/Customer.vue
@@ -2,7 +2,18 @@
 </script>
 
 <template>
-<div>Customer</div>
+<div class="grid grid-cols-6 mx-16 gap-20 mt-6 py-6">
+  <div class="bg-gray-100 col-span-2 p-3">
+    <p class="pt-1 text-lg">نام مشتری</p>
+    <p class="pt-1 ">تهران خ ولیعصر پلاک 10 واحد2</p>
+    <p class="pt-1 text-left">021-88558855</p>
+    <p class="pt-1 text-left">info@customer.com</p>
+  </div>
+  <div class="bg-gray-100 max-h-32"><span class="flex justify-center items-center h-full">1</span></div>
+  <div class="bg-gray-100 max-h-32"><span class="flex justify-center items-center h-full">2</span></div>
+  <div class="bg-gray-100 max-h-32"><span class="flex justify-center items-center h-full">3</span></div>
+  <div class="bg-gray-100 max-h-32"><span class="flex justify-center items-center h-full">4</span></div>
+</div>
 </template>
 
 
