7fc81625c93cf3328f27524db122f6165e5a0782
liva <ebammo20@gmail.com>
2023-01-05 17:24:21 +0330

    Add customer edit link

--- src/components/customer/Customer.vue
+++ src/components/customer/Customer.vue
@@ -3,11 +3,12 @@
 
 <template>
 <div class="grid grid-cols-6 mx-16 gap-20 mt-6 py-6">
-  <div class="bg-gray-100 col-span-2 p-3 rounded rounded-l">
+  <div class="bg-gray-100 col-span-2 p-3 rounded rounded-l relative">
     <p class="pt-1 text-lg">نام مشتری</p>
     <p class="pt-1 ">تهران خ ولیعصر پلاک 10 واحد2</p>
     <p class="pt-1 text-left">021-88558855</p>
     <p class="pt-1 text-left">info@customer.com</p>
+    <p class="absolute top-0 left-0 p-1 rounded-tl rounded-br text-red-500 text-xs cursor-pointer">ویرایش</p>
   </div>
   <div class="bg-gray-100 max-h-32 grid text-center content-center rounded rounded-l">
     <div class="px-6">
