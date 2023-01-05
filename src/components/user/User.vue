b6e048a618dff14a69aeebb27dd9f1b603ce6fc7
liva <ebammo20@gmail.com>
2023-01-05 18:28:38 +0330

    Add base element and layouts

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,10 +1,35 @@
 <script setup>
-
 </script>
 
 <template>
-  <div>User</div>
+  <div class="grid grid-cols-6 mx-16 gap-20 mt-6 py-6">
+    <div class="bg-gray-100 col-span-2 p-3 rounded rounded-l relative">
+      <p class="pt-1 text-lg">نام کاربر</p>
+      <p class="pt-1 ">کارشناس فروش روتین</p>
+    </div>
+    <div class="bg-gray-100 max-h-32 grid text-center content-center rounded rounded-l relative">
+      <div class="px-6">
+        <div class="pb-1 border-b ">سفارش</div>
+        <div class="font-bold absolute bottom-0 right-0 p-1">153</div>
+        <div class="font-bold absolute bottom-0 left-0 p-1">23%</div>
+      </div>
+    </div>
+    <div class="bg-gray-100 max-h-32 grid text-center content-center rounded rounded-l">
+      <div class="px-6">
+        <div class="pb-2 border-b ">فروش</div>
+        <div class="pt-2 font-bold">2500 کیلووات</div>
+      </div>
+    </div>
+    <div class="bg-gray-100 max-h-32 grid text-center content-center rounded rounded-l">
+      <div class="px-6">
+        <div class="pb-2 border-b ">فروش</div>
+        <div class="pt-2 font-bold">250,000,000 ریال</div>
+      </div>
+    </div>
+  </div>
 </template>
+
+
 
 <style scoped>
 
