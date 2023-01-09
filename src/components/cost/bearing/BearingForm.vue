edc0606ef6cdaed69d72353669a07f134ac5da3c
liva <ebammo20@gmail.com>
2023-01-09 13:14:10 +0330

    Remove description input field

--- src/components/cost/bearing/BearingForm.vue
+++ src/components/cost/bearing/BearingForm.vue
@@ -25,8 +25,7 @@ onDone(result => {
 </script>
 
 <template>
-<div class="p-8 rounded bg-gray-100">
-  <div v-if="msg.length > 0" class="absolute top-0 right-0 p-1 text-green-400">{{msg}}</div>
+<div class="p-6 rounded bg-gray-100">
   <div class="absolute top-0 left-0">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
          class="bg-red-500 text-white cursor-pointer rounded-tl w-4 h-4"
@@ -34,6 +33,7 @@ onDone(result => {
       <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
   </div>
+  <div v-if="msg.length > 0" class="py-3 text-green-400">{{msg}}</div>
   <div class="mb-2">
     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام برینگ</label>
     <input
@@ -41,12 +41,6 @@ onDone(result => {
         class="border border-gray-300 text-gray-900 text-sm rounded-lg
     focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text">
-  </div>
-  <div>
-    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">شرح</label>
-    <textarea v-model="description" class="block mb-2 p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300
-    focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-    dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
   </div>
   <div class="w-full text-left">
     <button class="bg-blue-500 text-white p-1.5 rounded" @click="createBearing">ذخیره</button>
