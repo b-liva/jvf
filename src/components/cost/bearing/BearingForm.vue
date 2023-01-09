4dc6070783dc997a9434ebd235c7b2e28489846a
liva <ebammo20@gmail.com>
2023-01-09 12:24:32 +0330

    Implement bearing mutation

--- src/components/cost/bearing/BearingForm.vue
+++ src/components/cost/bearing/BearingForm.vue
@@ -1,8 +1,20 @@
 <script setup>
 import {ref} from "vue";
+import {useMutation} from "@vue/apollo-composable";
+import {mutateBearing} from "../../../graphql/cost/mutation/cost/bearing.graphql";
+let name = ref('')
+let description = ref('');
 
-let title = ref('')
-let description = ref('')
+const {mutate: createBearing, loading, error, onDone} = useMutation(mutateBearing,
+    () => ({
+      variables: {
+        'name': name,
+      }
+    }));
+
+onDone(result => {
+  console.log('bearing created.')
+})
 </script>
 
 <template>
@@ -17,7 +29,7 @@ let description = ref('')
   <div class="mb-2">
     <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">نام برینگ</label>
     <input
-        v-model="title"
+        v-model="name"
         class="border border-gray-300 text-gray-900 text-sm rounded-lg
     focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600
     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text">
@@ -29,7 +41,7 @@ let description = ref('')
     dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></textarea>
   </div>
   <div class="w-full text-left">
-    <button class="bg-blue-500 text-white p-1.5 rounded">ذخیره</button>
+    <button class="bg-blue-500 text-white p-1.5 rounded" @click="createBearing">ذخیره</button>
   </div>
 </div>
 </template>
