a9053872f98e7effd27e724eae2ea3b309f1dfc1
liva <ebammo20@gmail.com>
2023-01-10 22:20:56 +0330

    Show data on income list

--- src/components/income/IncomeList.vue
+++ src/components/income/IncomeList.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {useRouter} from "vue-router";
 import {useIncomeStore} from "../../store/store";
+import JNumber from "../../utils/number";
 const router = useRouter();
 const incomeStore = useIncomeStore()
 </script>
@@ -18,22 +19,29 @@ const incomeStore = useIncomeStore()
           rounded-tr-lg rounded-bl-lg min-w-max">{{ income.node.number }}</div>
           <div class="col-start-13 text-green-700 p-1">{{income.node.owner.lastName}}</div>
         </div>
-        <div class="grid grid-cols-9 py-4">
+        <div class="grid grid-cols-8 py-4 gap-3">
           <div class="col-span-3">{{ income.node.customer.name }}</div>
           <div class="">
-            <div class="text-center">تعداد دستگاه</div>
-            <div class=" text-center text-green-500">***</div>
+            <div class="text-center">مبلغ دریافتی</div>
+            <div class=" text-center text-green-500">{{ new JNumber(income.node.amount).thousandSeparate() }}</div>
+          </div>
+          <div class="min-w-max">
+            <div class="text-center">تعداد تخصیص</div>
+            <div class="text-center ">
+              <span class="text-green-500">{{income.node.assignedCount}}</span> <span>مجوز</span></div>
+          </div>
+          <div class="min-w-max">
+            <div class="text-center">تخصیص داده شده</div>
+            <div class="text-center text-green-500">{{new JNumber(income.node.amountAssigned).thousandSeparate()}}</div>
           </div>
           <div class="">
-            <div class="text-center">کیلووات</div>
-            <div class="text-center text-green-500">***</div>
+            <div class="text-center">مانده</div>
+            <div class="text-center text-green-500">{{new JNumber(income.node.amountNotAssigned).thousandSeparate()}}</div>
           </div>
-          <div class="text-center">***</div>
-          <div class="col-span-3 text-right">
+          <div class="text-right">
             <div class="text-xs text-center text-gray-500 px-4 text-justify">
-              ****
+              تاریخ
             </div>
-
           </div>
         </div>
       </div>
